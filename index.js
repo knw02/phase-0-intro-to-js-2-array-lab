5411fb732af664aa55433a7be9d356ae62b26644
Kalie <kwatsonnov2002@gmail.com>
2023-10-14 00:15:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+let cats = ["Milo" , "Otis" , "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat (name) {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+}
+function appendCat(name) {
+    return cats.concat(name);
+}
+function prependCat(name) {
+    return [name, ...cats];
+}
+function removeLastCat(name) {
+    return cats.slice(0, -1);
+}
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
